a6484dd2b013516433aacedfddc25d25068714cf
jimmy-bull <jbull635@gmail.com>
2022-11-22 15:20:32 +0100

    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // // // \App\Models\User::factory(10)->create();
         // // $this->call(MainProductsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
         $this->call(PasswordResetsTableSeeder::class);
         $this->call(PersonalAccessTokensTableSeeder::class);
         // // $this->call(BidDataTableSeeder::class);
